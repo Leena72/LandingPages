088af3d171017aaec050c2d02eec546cafea1887
leena72 <leena.sharma72@gmail.com>
2021-03-17 04:34:40 +0530

    main changes

---
+++ js/animate.js
@@ -0,0 +1,96 @@
+function section1_animation(){
+    $('#section1 .heading-1').addClass('animation-s1-h1')
+    $('#section1 .heading-2').addClass('animation-s1-h2')
+    $('#section1 .content ,#section1 .line-img>img').addClass('animation-s1-content')
+}
+
+function section2_animation(){
+    $('#section2 .heading-1,#section2 .content').addClass('animation-s2-h1')
+    $('#section2 .heading-2').addClass('animation-s2-h2')
+    $('#section2 .line-img>img , #section2 .navigate-desk,#section2 .navigate-mob').addClass('animation-s2-btn')
+}
+
+function section3_animation(){
+    $('#section3 .frame-contentBody').addClass('animation-s3-framebody')
+    $('#section3 .board').addClass('animation-s3-board')
+}
+
+function section4_animation(){
+    $('#section4 .timer-img').addClass('animation-s4-timer')
+    $('#section4 .fr4-board').addClass('animation-s4-board')
+}
+function section5_animation(){
+    $('#section5 .answerBoxImg').addClass('animation-s5-answerBoxImg')
+    $('#section5 .heading-1 , #section5 .navigate-btn').addClass('animation-s5-h1')
+}
+
+function section7_animation(){
+    $('#section7 .timer-img').addClass('animation-s7-timer')
+    $('#section7 .fr4-board').addClass('animation-s7-board')
+}
+
+function section8_animation(){
+    $('#section8 .answerBoxImg-fr8 img').addClass('animation-s8-answerBoxImg-fr8')
+    $('#section8 .content ').addClass('animation-s8-content')
+}
+
+function section9_animation(){
+    $('#section9 .answerBoxImg-fr9 img').addClass('animation-s9-answerBoxImg-fr9')
+    $('#section9 .heading-1 ').addClass('animation-s9-h1')
+    $('#section9 .navigate-btn ').addClass('animation-s9-btn')
+}
+
+function section10_animation(){
+    $('#section10 .timer-img').addClass('animation-s10-timer')
+    $('#section10 .fr4-board').addClass('animation-s10-board')
+}
+
+function section11_animation(){
+    $('#section11 .answerBoxImg-fr11 img').addClass('animation-s11-answerBoxImg-fr11')
+    $('#section11 .content ').addClass('animation-s11-content')
+}
+
+function section12_animation(){
+    $('#section12 .timer-img').addClass('animation-s12-timer')
+    $('#section12 .fr4-board').addClass('animation-s12-board')
+}
+
+function section13_animation(){
+    $('#section13 .answerBoxImg-fr13 img').addClass('animation-s13-answerBoxImg-fr13')
+    $('#section13 .content ').addClass('animation-s13-h1')
+    $('#section13 .navigate-btn-fr13 ').addClass('animation-s13-btn')
+}
+
+function section14_animation(){
+    $('#section14 .fr14-heading').addClass('animation-s14-heading')
+    $('#section14 .navigate-btn ').addClass('animation-s14-btn')
+    $('#section14 .fr-14-content ').addClass('animation-s14-fr-14-content')
+    $('#section14 .graph img').addClass('animation-s14-graph')
+}
+
+function section15_animation(){
+    $('#section15 .fr14-heading').addClass('animation-s15-heading')
+    $('#section15 .navigate-btn-fr15 ').addClass('animation-s15-btn')
+    $('#section15 .fr-14-content ').addClass('animation-s15-fr-15-content')
+    $('#section15 .graph img').addClass('animation-s15-graph')
+}
+
+function section16_animation(){
+    $('#section16 .fr14-heading').addClass('animation-s16-heading')
+    $('#section16 .navigate-btn ').addClass('animation-s16-btn')
+    $('#section16 .Graph-83 ').addClass('animation-s16-Graph-83')
+    $('#section16 .graph-fr16 img').addClass('animation-s16-graph-fr16')
+}
+
+function section17_animation(){
+    $('#section17 .fr14-heading').addClass('animation-s17-heading')
+    $('#section17 .navigate-btn-fr17').addClass('animation-s17-btn')
+    $('#section17 .graph').addClass('animation-s17-graph')
+}
+
+function section18_animation(){
+    $('#section18 .frame-content').addClass('animation-s18-frame-content')
+    $('#section18 .button-section ').addClass('animation-s18-btn')
+    $('#section18 .carousel ').addClass('animation-s18-carousel')
+
+}
---
+++ js/navigate.js
@@ -0,0 +1,189 @@
+function section1_2(e){
+    $('#section1').slideUp('fast')
+    $('#section2').slideDown('fast'); 
+    section2_animation()
+}
+
+function section2_3(e){
+    $('#section2').slideUp('fast')
+    $('#section3').slideDown('fast'); 
+    section3_animation()
+}
+
+function section3_4(e){
+    $('#section3').slideUp('fast')
+    $('#section4').slideDown('fast'); 
+    section4_animation()
+}
+
+function section4_5(e){
+    $('#section4').slideUp('fast')
+    $('#section5').slideDown('fast'); 
+    section5_animation()
+}
+function section5_7(e){
+    $('#section5').slideUp('fast')
+    $('#section7').slideDown('fast'); 
+    section7_animation()
+}
+
+function section7_8(e){
+    $('#section7').slideUp('fast')
+    $('#section8').slideDown('fast'); 
+    section8_animation()
+}
+function section8_9(e){
+    $('#section8').slideUp('fast')
+    $('#section9').slideDown('fast'); 
+    section9_animation()
+}
+function section9_10(e){
+    $('#section9').slideUp('fast')
+    $('#section10').slideDown('fast'); 
+    section10_animation()
+}
+function section10_11(e){
+    $('#section10').slideUp('fast')
+    $('#section11').slideDown('fast'); 
+    section11_animation()
+}
+function section11_12(e){
+    $('#section11').slideUp('fast')
+    $('#section12').slideDown('fast'); 
+    section12_animation()
+}
+
+function section12_13(e){
+    $('#section12').slideUp('fast')
+    $('#section13').slideDown('fast'); 
+    section13_animation()
+}
+
+function section13_14(e){
+    $('#section13').slideUp('fast')
+    $('#section14').slideDown('fast'); 
+    section14_animation()
+}
+
+function section14_15(e){
+    $('#section14').slideUp('fast')
+    $('#section15').slideDown('fast'); 
+    section15_animation()
+}
+function section15_16(e){
+    $('#section15').slideUp('fast')
+    $('#section16').slideDown('fast'); 
+    section16_animation()
+   setTimeout(percentage_loader,2000)
+}
+function section16_17(e){
+    $('#section16').slideUp('fast')
+    $('#section17').slideDown('fast'); 
+    section17_animation()
+}
+function section17_18(e){
+    $('#section17').slideUp('fast')
+    $('#section18').slideDown('fast'); 
+    section18_animation()
+}
+  // hide and show 
+  $(document).ready(function () { 
+    section1_animation()
+    $('.home:not(:first)').slideUp('fast')
+    $("#section1-link").click( section1_2);
+    $("#section2-mob-link").click( section2_3);
+    $("#section2-desk-link").click( section2_3);
+    $("#section3-link").click( section3_4);
+    $("#section5-link").click( section5_7);
+    $("#section8-link").click( section8_9);
+    $("#section9-link").click( section9_10);
+    $("#section11-link").click(section11_12);
+    $("#section13-link").click(section13_14);
+    $("#section14-link").click(section14_15);
+    $("#section15-link").click(section15_16);
+    $("#section16-link").click(section16_17);
+    $("#section17-link").click(section17_18);
+
+  //--------------car movement----S4---------------
+  $("#s4-car").onPositionChanged(function () {
+    $('#s4-timercolor').css({
+        'width': $('#s4-car').position().left
+    })
+    if ($('#s4-car').position().left == 278 ||
+        $('#s4-car').position().left == 265 ||
+        $('#s4-car').position().left == 260 ||
+        $('#s4-car').position().left == 225 ||
+        $('#s4-car').position().left == 296 ||
+        $('#s4-car').position().left == 315 ||
+        $('#s4-car').position().left == 342 ||
+        $('#s4-car').position().left == 413  ||
+        $('#s4-car').position().left == 308) {
+            section4_5()
+    }
+    console.log("$('#s4-car').position().left",$('#s4-car').position().left)
+});
+
+//--------------car movement----S7---------------
+$("#s7-car").onPositionChanged(function () {
+    $('#s7-timercolor').css({
+        'width': $('#s7-car').position().left
+    })
+    if ($('#s7-car').position().left == 278 ||
+        $('#s7-car').position().left == 265 ||
+        $('#s7-car').position().left == 260 ||
+        $('#s7-car').position().left == 225 ||
+        $('#s7-car').position().left == 296 ||
+        $('#s7-car').position().left == 315 ||
+        $('#s7-car').position().left == 342 ||
+        $('#s7-car').position().left == 413  ||
+        $('#s7-car').position().left == 308) {
+            section7_8()
+    }
+    console.log("$('#s7-car').position().left",$('#s7-car').position().left)
+});
+//--------------car movement----S10---------------
+$("#s10-car").onPositionChanged(function () {
+    $('#s10-timercolor').css({
+        'width': $('#s10-car').position().left
+    })
+    if ($('#s10-car').position().left == 278 ||
+        $('#s10-car').position().left == 265 ||
+        $('#s10-car').position().left == 260 ||
+        $('#s10-car').position().left == 225 ||
+        $('#s10-car').position().left == 296 ||
+        $('#s10-car').position().left == 315 ||
+        $('#s10-car').position().left == 342 ||
+        $('#s10-car').position().left == 413  ||
+        $('#s10-car').position().left == 308) {
+            section10_11()
+    }
+    console.log("$('#s10-car').position().left",$('#s10-car').position().left)
+});
+//--------------car movement----S12---------------
+$("#s12-car").onPositionChanged(function () {
+    $('#s12-timercolor').css({
+        'width': $('#s12-car').position().left
+    })
+    if ($('#s12-car').position().left == 278 ||
+        $('#s12-car').position().left == 265 ||
+        $('#s12-car').position().left == 260 ||
+        $('#s12-car').position().left == 225 ||
+        $('#s12-car').position().left == 296 ||
+        $('#s12-car').position().left == 315 ||
+        $('#s12-car').position().left == 342 ||
+        $('#s12-car').position().left == 413  ||
+        $('#s12-car').position().left == 308) {
+            section12_13()
+    }
+    console.log("$('#s12-car').position().left",$('#s12-car').position().left)
+});
+
+$('#scroll-up-btn').click(function(){
+    window.location.reload()
+})
+
+});
+
+
+
+
